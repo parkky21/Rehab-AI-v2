830209d7fb381c92fd7b8bd57a94bb72a1982d2a
Parkky <parth.kale.dev@gmail.com>
2026-04-24 21:49:33 +0530

    feat - Add hip extension exercise

--- python-server/exercises/hip_extension.py
+++ python-server/exercises/hip_extension.py
@@ -6,7 +6,8 @@ from pipeline.feature_engine import calculate_angle_2d
 class StandingHipExtension(ExerciseBase):
     def __init__(self):
         super().__init__()
-        self.relevant_landmarks = [11, 23, 27]
+        # Left: 11, 23, 27 | Right: 12, 24, 28
+        self.relevant_landmarks = [11, 23, 27, 12, 24, 28]
         self.config = ExerciseConfig(
             target_rom=25.0,
             ideal_rep_time=4.0,
@@ -16,25 +17,53 @@ class StandingHipExtension(ExerciseBase):
             weight_tempo=0.25,
         )
         self.scorer.config = self.config
+        self.last_active_side = None
 
     def process(self, landmarks):
-        shoulder = landmarks[11]
-        hip = landmarks[23]
-        ankle = landmarks[27]
+        left_shoulder = landmarks[11]
+        left_hip = landmarks[23]
+        left_ankle = landmarks[27]
+        
+        right_shoulder = landmarks[12]
+        right_hip = landmarks[24]
+        right_ankle = landmarks[28]
 
-        angle = calculate_angle_2d(shoulder, hip, ankle)
-        self.rom_tracker.update(angle)
-        self.record_ml_frame(angle, landmarks)
+        left_angle = calculate_angle_2d(left_shoulder, left_hip, left_ankle)
+        right_angle = calculate_angle_2d(right_shoulder, right_hip, right_ankle)
+        
+        active_angle = left_angle if self.last_active_side == "left" else right_angle
+        if self.stage == "down" or self.stage == "up":
+             pass
+        else:
+             active_angle = (left_angle + right_angle) / 2.0
+
+        self.rom_tracker.update(active_angle)
+        self.record_ml_frame(active_angle, landmarks)
         self.rep_completed = False
 
-        if angle > 170:
+        if left_angle > 170 and right_angle > 170:
             self._on_rep_start()
             self.stage = "down"
-            self.feedback = "Kick leg backward"
-        if angle < 165 and self.stage == "down":  # Low threshold
-            self.stage = "up"
-            self.counter += 1
-            self._on_rep_complete()
-            self.feedback = f"Rep done! Score: {self.last_rep_scores['final_score']}"
+            self.feedback = "Kick alternating leg backward"
+            
+        elif left_angle < 165 and right_angle >= 165 and self.stage == "down":
+            if self.last_active_side != "left":
+                self.stage = "up"
+                self.counter += 1
+                self.last_active_side = "left"
+                self._on_rep_complete()
+                self.feedback = f"Left rep done! Score: {self.last_rep_scores['final_score']}"
+            else:
+                self.feedback = "Use right leg to alternate"
 
-        return self.counter, self.stage, self.feedback, {"angle": angle, "points": [shoulder, hip, ankle]}
+        elif right_angle < 165 and left_angle >= 165 and self.stage == "down":
+            if self.last_active_side != "right":
+                self.stage = "up"
+                self.counter += 1
+                self.last_active_side = "right"
+                self._on_rep_complete()
+                self.feedback = f"Right rep done! Score: {self.last_rep_scores['final_score']}"
+            else:
+                self.feedback = "Use left leg to alternate"
+
+        return self.counter, self.stage, self.feedback, {"angle": active_angle, "points": [left_shoulder, left_hip, left_ankle, right_shoulder, right_hip, right_ankle]}
